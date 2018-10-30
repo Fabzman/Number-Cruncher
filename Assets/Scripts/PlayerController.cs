d6de956c58090f116c6287ef6c66ab333e7c5548
Fabzman <Fabian.Est.Figueroa@gmail.com>
2018-10-30 17:17:00 +1100

    Animations, Gameobjects and Scripting
    Created player movement and enemy scripting, created animations for player fists, created more background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float Speed;
+    public Rigidbody rb;
+    private Vector3 movement;
+    private Vector3 moveSpeed;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("Not Punching");
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * Speed, rb.velocity.y, Input.GetAxisRaw("Vertical") * Speed);
+        //movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveSpeed = movement * Speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Punching");
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetTrigger("Not Punching");
+        }
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    rigidBody.velocity = moveSpeed;
+    //}
 }
