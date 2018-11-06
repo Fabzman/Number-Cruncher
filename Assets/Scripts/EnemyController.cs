89e83e480ca48f5516d621239622a8d6442d8e42
Fabzman <Fabian.Est.Figueroa@gmail.com>
2018-11-06 18:38:17 +1100

    Enemy Scripting
    Successfully made enemy move towards player gameobject

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
+    private PlayerController _player;
+    public float enemySpeed;
+    public Transform player;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
+        _player = GameObject.Find("Player").GetComponent<PlayerController>();
+        player = GameObject.Find("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        float step = enemySpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
